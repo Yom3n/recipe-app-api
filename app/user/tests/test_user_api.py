22d0ab6d1afc3c672ab5bffa3c30e9f60c0f40b6
kamilek41@gmail.com <adgjmptw9>
2021-12-04 14:18:58 +0100

    Added working list tags enpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -122,11 +122,13 @@ class PrivateUserApiTests(TestCase):
 
     def test_update_user_profile(self):
         """Test updateing the user profile for authenticated user"""
+        # Arrange
         payload = {'name': 'new name', 'password': 'password123'}
+        # Act
         res = self.client.patch(ME_URL, payload)
-        # Fetches user from db
+        # Equivalent for Users.objects.get(res[id])
         self.user.refresh_from_db()
-
+        # Assert
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
